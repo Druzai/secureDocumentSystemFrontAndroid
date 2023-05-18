0a4e3ed2d9d4135ff38aa7c5452e9a6b052c65eb
Druzai <g9code@yandex.ru>
2023-05-19 00:47:23 +0300

    Added button logout Fixed layouts

--- app/src/main/java/ru/mirea/documenteditor/ui/activities/MainActivity.java
+++ app/src/main/java/ru/mirea/documenteditor/ui/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package ru.mirea.documenteditor.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -18,12 +24,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import ru.mirea.documenteditor.R;
 import ru.mirea.documenteditor.databinding.ActivityMainBinding;
+import ru.mirea.documenteditor.ui.activities.auth.AuthActivity;
+import ru.mirea.documenteditor.ui.activities.welcome.WelcomeActivity;
+import ru.mirea.documenteditor.ui.fragments.gallery.GalleryFragment;
+import ru.mirea.documenteditor.ui.fragments.home.HomeFragment;
+import ru.mirea.documenteditor.ui.fragments.slideshow.SlideshowFragment;
+import ru.mirea.documenteditor.util.Constants;
 import ru.mirea.documenteditor.util.PreferenceManager;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private DrawerLayout drawerLayout;
     private ActivityMainBinding binding;
 
     @Override
@@ -37,26 +50,30 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                PreferenceManager pref = PreferenceManager.getInstance();
-                pref.clear();
-                Snackbar.make(view, "Cleared preferences!", Snackbar.LENGTH_SHORT)
+                Snackbar.make(view, "Some action!", Snackbar.LENGTH_SHORT)
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
+        drawerLayout = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setOpenableLayout(drawer)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+        )
+                .setOpenableLayout(drawerLayout)
                 .build();
 //        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
 //        NavController navController = navHostFragment.getNavController();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        setNavigationViewListener();
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.textView);
+        navUsername.setText(PreferenceManager.getInstance().getString(Constants.USER_NAME));
     }
 
     @Override
@@ -71,5 +88,16 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.getMenu().findItem(R.id.nav_logout).setOnMenuItemClickListener(menuItem -> {
+            PreferenceManager pref = PreferenceManager.getInstance();
+            pref.clear();
+            Intent intent = new Intent(this, AuthActivity.class);
+            startActivity(intent);
+            return true;
+        });
     }
 }
